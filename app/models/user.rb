11c770bd5455ee9f4a6582819027f0503017968f
Deborah Barcenas <deborahbarcenas@deborahs-mbp.attlocal.net>
2020-07-30 13:47:04 -0400

    user sign in validates

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,11 @@
 class User < ActiveRecord::Base
   has_secure_password
-  validates :email, :presence => true
   has_many :user_medications
   has_many :medications, through: :user_medications
   has_many :entries
+
+  validates :email, :presence => true
+  validates :username, uniqueness: true, presence: true
+  validates :password, presence: true
+  validates :password, confirmation: {case_sensitive: true}
 end
